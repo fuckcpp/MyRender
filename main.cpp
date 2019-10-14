99b7c2730d6c782194d1bff04fb0a6f9d3501eed
fuckcpp <m18588290231@163.com>
2019-10-13 23:34:48 -0700

    optimize line func

--- main.cpp
+++ main.cpp
@@ -15,14 +15,24 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
+	
+	int dy=y1-y0;
+	int dx=x1-x0;
+	float slope=dy/(float)dx;
+	float yadd=0;
+	int y=y0;
 	for (int x = x0; x <= x1; x++) {
-		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0 * (1. - t) + y1 * t;
 		if (steep) {
 			image.set(y, x, color); // if transposed, de−transpose 
 		}
 		else {
 			image.set(x, y, color);
+		}
+		yadd+=slope;
+		if(yadd>0.5)
+		{
+			y+=y1>y0?1.:-1.;	
+			yadd-=1.;
 		}
 	}
 }
